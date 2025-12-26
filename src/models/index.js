45d10d81f88f48eebc297d38b6d2b59d34b3e811
shekhar-sainii <shekharsingh7900@gmail.com>
2025-12-26 23:49:13 +0530

    code

--- src/models/index.js
+++ src/models/index.js
@@ -18,14 +18,10 @@ const db = {};
 // }
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",   // ✅ MUST BE HERE
-    logging: false,
-  }
+  config.database,
+  config.username,
+  config.password,
+  config
 );
 
 fs
