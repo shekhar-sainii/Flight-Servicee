e794543ceee8d96561eb7407ccd34397eb89c4bf
shekhar-sainii <shekharsingh7900@gmail.com>
2025-12-26 22:43:55 +0530

    Code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const express = require("express")
 const {ServerConfig, Logger} = require("./config")
 const apiRoutes = require("./routes")
+const morgan = require('morgan')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(morgan("combined"))
 
 app.use("/api", apiRoutes)
-
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
     // Logger.info("Successfully Started the server", {})
